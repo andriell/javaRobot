8746ee5c61d87d1446600b6a962db99b8a58318d
andriell <root.andriell@gmail.com>
2015-01-19 00:31:58 +0400

    Start

--- src/Main.java
+++ src/Main.java
@@ -192,11 +192,9 @@ public class Main {
     // во время боя 217,179,179
     public static void waitBattle1Complite() {
         RobotHelper.delay(60000);
-        RobotHelper.delay(60000);
     }
 
     public static void waitBattle2Complite() {
-        RobotHelper.delay(60000);
         RobotHelper.delay(60000);
     }
 
